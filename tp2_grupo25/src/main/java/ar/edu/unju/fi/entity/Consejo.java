6639a1fb26beb7aa8da2f31f2f5b3b674ec6ecc4
Mauricio Nehuen Velazquez <mauri_i9070@hotmail.com>
2023-06-28 16:29:52 -0300

    Se agrego la entidad Consejo, se logro la conexion con la base de datos

--- tp2_grupo25/src/main/java/ar/edu/unju/fi/entity/Consejo.java
+++ tp2_grupo25/src/main/java/ar/edu/unju/fi/entity/Consejo.java
@@ -3,16 +3,32 @@ package ar.edu.unju.fi.entity;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 
 @Component
+@Entity
+@Table(name = "Consejos")
 public class Consejo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cjo_id")
+    private Long id;
+
+    @Column(name = "cjo_titulo", length = 30, nullable = false)
     @NotEmpty(message = "El campo titulo es obligatorio")
     private String titulo;
 
+    @Column(name = "cjo_imgUrl")
     private String imgUrl;
 
+    @Column(name = "cjo_consejo", nullable = false)
     @Length(min = 20, message = "El consejo debe tener almenos 20 caracteres")
     private String consejo;
 
