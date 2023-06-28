d76e00b65d84a128373b36efdd172264423f46de
Mauricio Nehuen Velazquez <mauri_i9070@hotmail.com>
2023-06-28 16:09:07 -0300

    Se agrego la entidad sucursal, se logro la conexion con la base de datos

--- tp2_grupo25/src/main/java/ar/edu/unju/fi/entity/Sucursal.java
+++ tp2_grupo25/src/main/java/ar/edu/unju/fi/entity/Sucursal.java
@@ -2,19 +2,38 @@ package ar.edu.unju.fi.entity;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 
 @Component
+@Entity
+@Table(name="sucursales")
 public class Sucursal {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "suc_id")
+    private Long id;
+
+    @Column(name = "suc_nombre", length = 30, nullable = false)
     @NotEmpty(message = "El nombre es obligatorio")
     private String nombre;
 
+    @Column(name = "suc_encargado", length = 20, nullable = false)
     @NotEmpty(message = "El encargado es obligatorio")
     private String encargadoNombre;
 
+    @Column(name = "suc_direccion", length = 40, nullable = false)
     @NotEmpty(message = "La direccion es obligatoria")
     private String direccion;
+
+    @Column(name = "suc_estado")
+    private boolean estado;
     
     public Sucursal() {
     }
